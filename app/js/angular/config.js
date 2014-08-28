d8853a141a12b29d6555328808e50c870b0f6bc6
benjamin.cabanes <benjamin.cabanes@cossette.com>
2014-08-28 11:59:52 -0400

    master: add sample application

--- app/js/angular/config.js
+++ app/js/angular/config.js
@@ -1,4 +1,10 @@
-angular.module("app").config(["$interpolateProvider", function ($interpolateProvider) {
-    $interpolateProvider.startSymbol("[[");
-    $interpolateProvider.endSymbol("]]");
-}]);
+/* globale angular */
+
+/**
+ * Change the default token makers for AngularJS
+ * from '{{}}' to '[[]]'
+ */
+// angular.module('app').config(['$interpolateProvider', function($interpolateProvider){
+//  $interpolateProvider.startSymbol('[[');
+//  $interpolateProvider.endSymbol(']]');
+// }]);
